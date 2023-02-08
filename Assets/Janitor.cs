c1d2fc5111b2b9b37a5bcb3f3bf4538ec7e7b762
phucnds <42528099+phucnds@users.noreply.github.com>
2023-02-08 08:40:44 +0700

    !Init

---
+++ Assets/Janitor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Janitor : GAgent
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        SubGoal s1 = new SubGoal("clean", 1, false);
+        goals.Add(s1, 3);
+
+        
+    }
+
+    
+
+}
